0dd1a4c1c7ce37970f866ab10cf6a049f7511567
ojm <junmyung2020@gmail.com>
2024-11-29 15:57:00 +0900

    Update project file.
    Update project file.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', pageName: 'home.ejs' });
+});
+
+router.get('/book', function(req, res, next) {
+  res.render('index', { title: '도서검색', pageName: 'book/bookList.ejs' });
 });
 
 module.exports = router;
